e5105774e5caa739b967613a527f6e915b095266
Thainara Furforo <thaifurforo@gmail.com>
2022-04-07 22:20:18 -0300

    First commit

--- verify_surrounds.py
+++ verify_surrounds.py
@@ -3,3 +3,19 @@
     parameters: posição em tupla (l,c) * com base na função mover
     return: boolean
 """
+from mapper import *
+
+
+verificar_arredores():
+    return (c, l)
+
+arredores = verificar_arredores()
+
+mover(arredores):
+    mover para(c, l)
+    posicao_antiga == "."
+    if (c, l) == "S"
+    vitória
+    elif (c, l) == "."
+    remove(c, l) da pilha
+    (c, l) = "X"
