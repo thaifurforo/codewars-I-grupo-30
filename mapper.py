f38ec3f864cd9523805f4254918a3615b482ce33
Yuri <aoyamabrasil@gmail.com>
2022-04-08 10:44:33 -0300

    opinião sobre alteração

--- mapper.py
+++ mapper.py
@@ -16,3 +16,24 @@ def read_map_file() -> list:
             map.append(line.strip())
 
     return map
+
+
+"""
+Yuri - opinião sobre alteração:
+
+def print_map(lab_map: list) -> None:
+    for line in lab_map:
+        print(line)
+
+    return None
+
+
+def read_map_file() -> list:
+    lab_map = []
+    with open('map.txt', 'r') as file:
+        for line in file:
+            lab_map.append(line.strip())
+
+    return lab_map
+
+"""
