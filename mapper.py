89ca2999cc2054516286bf8bc3aff9c186a4a4a2
Thainara Furforo <thaifurforo@gmail.com>
2022-04-08 20:11:11 -0300

    Sugestões Thai

--- mapper.py
+++ mapper.py
@@ -18,9 +18,7 @@ def read_map_file() -> list:
     return map
 
 
-
-
-#opinião sobre mudanças:
+# opinião sobre mudanças:
 def read_map_file() -> list:
     global MAZE
     MAZE = []
@@ -31,9 +29,33 @@ def read_map_file() -> list:
 
     return MAZE
 
+
 def print_map() -> None:
     for line in MAZE:
         print(line)
 
     return None
 
+# sugestão thai - pra que crie lista de listas que fica mais fácil de buscar o index quando fizer o move. a impressão está fazendo caracter a caracter, aí mesmo sendo listas de listas fica bonita no print
+
+
+def print_map(map: list) -> None:
+    for line in map:
+        line_str = ''
+        for char in line:
+            line_str += char
+        print(line_str)
+
+    return None
+
+
+def read_map_file() -> list:
+    with open('map.txt', 'r') as file:
+        maze = [[char for char in line] for line in file]
+    for line in maze:
+        if len(line) <= 20:
+            break
+        else:
+            line.pop()
+
+    return maze
